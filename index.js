685b808420083b660d45e612d1228ba705fc41a7
Pankaj Wakchaure <pankaj.corp@gmail.com>
2022-08-27 15:26:09 +0530

    redux basic setup completed

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const redux = require("redux");
+
+// defining action
 const CAKE_ORDERED = "CAKE_ORDERED";
 
+// action creator
 function orderCake() {
   return {
     type: CAKE_ORDERED,
@@ -7,10 +11,12 @@ function orderCake() {
   };
 }
 
+// initial state
 const initialState = {
   numOfCakes: 10,
 };
 
+// creating reducer
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
@@ -22,3 +28,23 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+// creating store
+const store = redux.createStore(reducer);
+
+console.log("Initial state: ", store.getState());
+
+// subscribigng to store
+const unsubscribe = store.subscribe(() => {
+  console.log("Update state: ", store.getState());
+});
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+// unsubscribigng from store
+unsubscribe();
+
+// since now we are unscribed this will not work anymore
+store.dispatch(orderCake());
