98b0e5d4c1881dd0d0bf0d01cb300d728c2087f2
Pankaj Wakchaure <pankaj.corp@gmail.com>
2022-08-27 16:43:52 +0530

    multiple reducers

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const redux = require("redux");
 // defining action
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const PIZZA_ORDERED = "PIZZA_ORDERED";
+const PIZZA_RESTOCKED = "PIZZA_RESTOCKED";
 
 // action creator
 function orderCake() {
@@ -19,13 +21,33 @@ function restockCake(quantity = 1) {
   };
 }
 
+// action creator
+function orderPizza() {
+  return {
+    type: PIZZA_ORDERED,
+    payload: 1,
+  };
+}
+
+function restockPizza(quantity = 1) {
+  return {
+    type: PIZZA_RESTOCKED,
+    payload: quantity,
+  };
+}
+
 // initial state
-const initialState = {
+const initialCakeState = {
   numOfCakes: 10,
 };
 
+// initial state
+const initialPizzaState = {
+  numOfPizzas: 20,
+};
+
 // creating reducer
-const reducer = (state = initialState, action) => {
+const reducerCake = (state = initialCakeState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
       return {
@@ -43,24 +65,42 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-// creating store
-const store = redux.createStore(reducer);
+// creating reducer
+const reducerPizza = (state = initialPizzaState, action) => {
+  switch (action.type) {
+    case PIZZA_ORDERED:
+      return {
+        ...state,
+        numOfPizzas: state.numOfPizzas - 1,
+      };
 
+    case PIZZA_RESTOCKED:
+      return {
+        ...state,
+        numOfPizzas: state.numOfPizzas + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = redux.combineReducers({
+  cake: reducerCake,
+  pizza: reducerPizza,
+});
+
+// creating store
+const store = redux.createStore(rootReducer);
 console.log("Initial state: ", store.getState());
 
 // subscribigng to store
 const unsubscribe = store.subscribe(() => {
-  console.log("Update state: ", store.getState());
+  console.log("Updated state: ", store.getState());
 });
-
-// store.dispatch(orderCake());
-// store.dispatch(orderCake());
-// store.dispatch(orderCake());
-// store.dispatch(restockCake(3));
 
 // binding action creators
 const actions = redux.bindActionCreators(
-  { orderCake, restockCake },
+  { orderCake, restockCake, orderPizza, restockPizza },
   store.dispatch
 );
 
@@ -68,6 +108,10 @@ actions.orderCake();
 actions.orderCake();
 actions.orderCake();
 actions.restockCake(3);
+actions.orderPizza();
+actions.orderPizza();
+actions.orderPizza();
+actions.restockPizza(3);
 
 // unsubscribigng from store
 unsubscribe();
